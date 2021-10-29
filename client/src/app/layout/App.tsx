c5874464987154e7e494de7bebcb8e8fbf59499a
Patrice Beaule <patricebeaule@gmail.com>
2021-10-29 10:22:16 -0400

    Enf of section 5

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -2,6 +2,7 @@
 import { Container, CssBaseline,createTheme, ThemeProvider } from '@mui/material';
 import { useState } from 'react';
 import { Route } from 'react-router';
+import { ToastContainer } from 'react-toastify';
 import AboutPage from '../../features/about/AboutPage';
 import Catalog from '../../features/catalog/Catalog';
 import ProductDetails from '../../features/catalog/ProductDetails';
@@ -9,6 +10,10 @@ import ContactPage from '../../features/contact/ContactPage';
 import HomePage from '../../features/home/HomePage';
 
 import Header from './Header';
+import 'react-toastify/dist/ReactToastify.css'
+import ServerError from '../errors/ServerError';
+import NotFound from '../errors/NotFound';
+import { Switch } from 'react-router-dom';
 
 
 function App() {
@@ -29,14 +34,19 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position='bottom-right' hideProgressBar />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/catalog' component={Catalog} />
-        <Route path='/catalog/:id' component={ProductDetails} />
-        <Route path='/about' component={AboutPage} />
-        <Route path='/contact' component={ContactPage} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/catalog' component={Catalog} />
+          <Route path='/catalog/:id' component={ProductDetails} />
+          <Route path='/about' component={AboutPage} />
+          <Route path='/contact' component={ContactPage} />
+          <Route path='/server-error' component={ServerError} />
+          <Route component={NotFound} />
+        </Switch>
       </Container>
     </ThemeProvider>
   );
