a05edc11e4102e6717daed54a28dee533e7bc020
Patrice Beaule <patricebeaule@gmail.com>
2021-11-16 15:27:58 -0500

    End of section 10

--- client/src/features/basket/BasketSummary.tsx
+++ client/src/features/basket/BasketSummary.tsx
@@ -9,9 +9,18 @@ import {
 import { useAppSelector } from '../../app/store/configureStore';
 import { currencyFormat } from '../../app/util/util';
 
-export default function BasketSummary() {
-  const { basket } = useAppSelector(state => state.basket);
-  const subtotal = basket?.items.reduce((sum, item) => sum + (item.quantity * item.price), 0) ??  0;
+interface Props {
+  subtotal?: number;
+}
+
+export default function BasketSummary({ subtotal }: Props) {
+  const { basket } = useAppSelector((state) => state.basket);
+  if (subtotal === undefined)
+    subtotal =
+      basket?.items.reduce(
+        (sum, item) => sum + item.quantity * item.price,
+        0
+      ) ?? 0;
   const deliveryFee = subtotal > 10000 ? 0 : 500;
 
   return (
@@ -29,7 +38,9 @@ export default function BasketSummary() {
             </TableRow>
             <TableRow>
               <TableCell colSpan={2}>Total</TableCell>
-              <TableCell align='right'>{currencyFormat(subtotal + deliveryFee)}</TableCell>
+              <TableCell align='right'>
+                {currencyFormat(subtotal + deliveryFee)}
+              </TableCell>
             </TableRow>
             <TableRow>
               <TableCell>
